405aacf42a48045f0a1f5f73c2b58ffb27584449
luanebsg <luanebsg@gmail.com>
2025-06-11 23:48:49 -0300

    xx

--- MiscellaneousUtilities/ForFriends/WeddingChecklist/tailwind.config.js
+++ MiscellaneousUtilities/ForFriends/WeddingChecklist/tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
